35fd7f6cd70ebb7708bb0c4a131ced4a0ded35f6
Sudip Paudel <sudip793@gmail.com>
2025-02-19 11:25:02 +0545

    suspense added

--- src/app/booking_details/page.js
+++ src/app/booking_details/page.js
@@ -3,27 +3,29 @@
 import Image from "next/image";
 import ArrowRight from '@assets/Icons/arrow-right.svg';
 import BookingDetailsForm from "./form";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Suspense } from "react";
 
 export default function BookingDetails() {
   const router = useRouter();
-  const params = useSearchParams();
 
   return(
-    <div className="bg-[#1E1E1E] h-screen booking-details relative">
-      <div className="pt-[65px] px-[20px]">
-        <div onClick={() => router.back()} className="pb-[24px]">
-          <Image
-            src={ArrowRight}
-            width={12}
-            height={8}
-            style={{width: 'auto', height: 'auto'}}
-            className="rotate-180 cursor-pointer"
-            alt="arrow-right"
-          />
+    <Suspense>
+      <div className="bg-[#1E1E1E] h-screen booking-details relative">
+        <div className="pt-[65px] px-[20px]">
+          <div onClick={() => router.back()} className="pb-[24px]">
+            <Image
+              src={ArrowRight}
+              width={12}
+              height={8}
+              style={{width: 'auto', height: 'auto'}}
+              className="rotate-180 cursor-pointer"
+              alt="arrow-right"
+            />
+          </div>
+          <BookingDetailsForm />
         </div>
-        <BookingDetailsForm />
       </div>
-    </div>
+    </Suspense>
   )
 }
